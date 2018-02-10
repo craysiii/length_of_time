4b2a31f549fd3007e807cd96c14b5985479c617c
Charles Ray Shisler III <charles@cray.io>
2018-02-09 23:40:10 -0800

    Implement unit conversion code.

--- lib/length_of_time.rb
+++ lib/length_of_time.rb
@@ -9,44 +9,54 @@ class Numeric
   SECONDS_PER_WEEK        = 604_800
   SECONDS_PER_FORTNIGHT   = 1_209_600
 
-  def millisecond
-    self * SECONDS_PER_MILLISECOND
+  LENGTHS_OF_TIME = {
+    milliseconds: SECONDS_PER_MILLISECOND,
+    seconds: SECONDS_PER_SECOND,
+    minutes: SECONDS_PER_MINUTE,
+    hours: SECONDS_PER_HOUR,
+    days: SECONDS_PER_DAY,
+    weeks: SECONDS_PER_WEEK,
+    fortnights: SECONDS_PER_FORTNIGHT
+  }.freeze
+
+  def millisecond(into: :seconds)
+    self * SECONDS_PER_MILLISECOND / LENGTHS_OF_TIME[into]
   end
 
   alias milliseconds millisecond
 
-  def second
-    self
+  def second(into: :seconds)
+    self * SECONDS_PER_SECOND / LENGTHS_OF_TIME[into]
   end
 
   alias seconds second
 
-  def minute
-    self * SECONDS_PER_MINUTE
+  def minute(into: :seconds)
+    self * SECONDS_PER_MINUTE / LENGTHS_OF_TIME[into]
   end
 
   alias minutes minute
 
-  def hour
-    self * SECONDS_PER_HOUR
+  def hour(into: :seconds)
+    self * SECONDS_PER_HOUR / LENGTHS_OF_TIME[into]
   end
 
   alias hours hour
 
-  def day
-    self * SECONDS_PER_DAY
+  def day(into: :seconds)
+    self * SECONDS_PER_DAY / LENGTHS_OF_TIME[into]
   end
 
   alias days day
 
-  def week
-    self * SECONDS_PER_WEEK
+  def week(into: :seconds)
+    self * SECONDS_PER_WEEK / LENGTHS_OF_TIME[into]
   end
 
   alias weeks week
 
-  def fortnight
-    self * SECONDS_PER_FORTNIGHT
+  def fortnight(into: :seconds)
+    self * SECONDS_PER_FORTNIGHT / LENGTHS_OF_TIME[into]
   end
 
   alias fortnights fortnight
