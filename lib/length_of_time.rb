01d0094aa86b3ce8fb2e16d4ba45414bba231735
Charles Ray Shisler III <charles@cray.io>
2018-02-11 12:03:33 -0800

    Refactor code to take advantage of define_method.

--- lib/length_of_time.rb
+++ lib/length_of_time.rb
@@ -9,55 +9,18 @@ class Numeric
   SECONDS_PER_WEEK        = 604_800
   SECONDS_PER_FORTNIGHT   = 1_209_600
 
-  LENGTHS_OF_TIME = {
+  TIME = {
     milliseconds: SECONDS_PER_MILLISECOND,
-    seconds: SECONDS_PER_SECOND,
-    minutes: SECONDS_PER_MINUTE,
-    hours: SECONDS_PER_HOUR,
-    days: SECONDS_PER_DAY,
-    weeks: SECONDS_PER_WEEK,
-    fortnights: SECONDS_PER_FORTNIGHT
+    seconds:      SECONDS_PER_SECOND,
+    minutes:      SECONDS_PER_MINUTE,
+    hours:        SECONDS_PER_HOUR,
+    days:         SECONDS_PER_DAY,
+    weeks:        SECONDS_PER_WEEK,
+    fortnights:   SECONDS_PER_FORTNIGHT
   }.freeze
 
-  def millisecond(into: :seconds)
-    self * SECONDS_PER_MILLISECOND / LENGTHS_OF_TIME[into]
+  TIME.each_key do |method|
+    define_method(method) { |into: :seconds| self * TIME[method] / TIME[into] }
+    alias_method method.to_s.chop, method
   end
-
-  alias milliseconds millisecond
-
-  def second(into: :seconds)
-    self * SECONDS_PER_SECOND / LENGTHS_OF_TIME[into]
-  end
-
-  alias seconds second
-
-  def minute(into: :seconds)
-    self * SECONDS_PER_MINUTE / LENGTHS_OF_TIME[into]
-  end
-
-  alias minutes minute
-
-  def hour(into: :seconds)
-    self * SECONDS_PER_HOUR / LENGTHS_OF_TIME[into]
-  end
-
-  alias hours hour
-
-  def day(into: :seconds)
-    self * SECONDS_PER_DAY / LENGTHS_OF_TIME[into]
-  end
-
-  alias days day
-
-  def week(into: :seconds)
-    self * SECONDS_PER_WEEK / LENGTHS_OF_TIME[into]
-  end
-
-  alias weeks week
-
-  def fortnight(into: :seconds)
-    self * SECONDS_PER_FORTNIGHT / LENGTHS_OF_TIME[into]
-  end
-
-  alias fortnights fortnight
 end
