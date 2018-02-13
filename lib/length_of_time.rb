c0dd0e67f5bc29be6016a1de8d43649dbd228d04
Charles Ray Shisler III <charles@cray.io>
2018-02-12 22:54:51 -0800

    Refactor Numeric monkey-patch out of length_of_time.rb.

--- lib/length_of_time.rb
+++ lib/length_of_time.rb
@@ -1,28 +1,2 @@
 require 'length_of_time/version'
-
-class Numeric
-  SECONDS_PER_MILLISECOND = 1.0 / 1000
-  SECONDS_PER_SECOND      = 1
-  SECONDS_PER_MINUTE      = 60
-  SECONDS_PER_HOUR        = 3_600
-  SECONDS_PER_DAY         = 86_400
-  SECONDS_PER_WEEK        = 604_800
-  SECONDS_PER_FORTNIGHT   = 1_209_600
-
-  TIME = {
-    milliseconds: SECONDS_PER_MILLISECOND,
-    seconds:      SECONDS_PER_SECOND,
-    minutes:      SECONDS_PER_MINUTE,
-    hours:        SECONDS_PER_HOUR,
-    days:         SECONDS_PER_DAY,
-    weeks:        SECONDS_PER_WEEK,
-    fortnights:   SECONDS_PER_FORTNIGHT
-  }.freeze
-
-  TIME.each_key do |method|
-    define_method(method) do |opt = { in: :seconds }|
-      self * TIME[method] / TIME[opt[:in]]
-    end
-    alias_method method.to_s.chop, method
-  end
-end
+require 'length_of_time/numeric'
