f09d7da51e17df1c76ce1aee144fa54ac76c0c6a
Charles Ray Shisler III <charles@cray.io>
2018-02-18 11:06:59 -0800

    Consolidate class constants into TIME.

--- lib/length_of_time/numeric.rb
+++ lib/length_of_time/numeric.rb
@@ -1,20 +1,12 @@
 class Numeric
-  SECONDS_PER_MILLISECOND = 1.0 / 1000
-  SECONDS_PER_SECOND      = 1.0
-  SECONDS_PER_MINUTE      = 60.0
-  SECONDS_PER_HOUR        = 3_600.0
-  SECONDS_PER_DAY         = 86_400.0
-  SECONDS_PER_WEEK        = 604_800.0
-  SECONDS_PER_FORTNIGHT   = 1_209_600.0
-
   TIME = {
-    milliseconds: SECONDS_PER_MILLISECOND,
-    seconds:      SECONDS_PER_SECOND,
-    minutes:      SECONDS_PER_MINUTE,
-    hours:        SECONDS_PER_HOUR,
-    days:         SECONDS_PER_DAY,
-    weeks:        SECONDS_PER_WEEK,
-    fortnights:   SECONDS_PER_FORTNIGHT
+    milliseconds: 1.0 / 1000,
+    seconds:      1.0,
+    minutes:      60.0,
+    hours:        3_600.0,
+    days:         86_400.0,
+    weeks:        604_800.0,
+    fortnights:   1_209_600.0
   }.freeze
 
   TIME.each_key do |method|
